e613bd07b7d4dcc2bbca342572fc8e21cb32567a
Vaddex <Gosvlad236@gmail.com>
2025-07-17 14:29:51 +0300

    added background image

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
+import MainPage from "./MainPage/MainPage";
 import MovieCard from "./MovieCard/MovieCard";
 
 function App() {
     return (
         <>
             <div>
+                <MainPage />
                 <MovieCard />
             </div>
         </>
--- src/components/MainPage/MainPage.jsx
+++ src/components/MainPage/MainPage.jsx
@@ -1,3 +1,12 @@
-const MainPage = () => {};
+import css from "./MainPage.module.css";
 
+const MainPage = () => {
+    return (
+        <div className={css.mainPage}>
+            <h1>🎥 Вітаємо у Кіно Квізі!</h1>
+            <p>Готові кинути виклик своєму кінознацтву?</p>
+            {/* Тут можуть бути кнопки, карточки, навігація */}
+        </div>
+    );
+};
 export default MainPage;
